fcef39cd8cccad9dac61d0129bef3e51bd85907d
Muhtasim <mts.ahmed.1010@gmail.com>
2023-04-03 18:02:06 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(listOfNames, eventName){
+    const newArray= []
+    for(let i=0; i < listOfNames.length; i++){
+        let message   
+        message = `Thank you, ${listOfNames[i]}, for the wonderful ${eventName} gift!`
+        newArray[i] = message     
+    }
+    return newArray
+}
+function countDown(number){
+    while (number!= -1){
+        console.log(number)
+        number--
+    }
+}
